e1f008d7bb52593dcbae9082cc19ef0da29dfea7
0x0ff <postmaster@0x0ff.info>
2013-08-19 14:55:39 -0400

    logfile supervision added

---
+++ cmatrix-1.2a-0x0ff.info-edit/config.h
@@ -0,0 +1,58 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader 2.13.  */
+
+/* Define as the return type of signal handlers (int or void).  */
+#define RETSIGTYPE void
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define this if your curses library has use_default_colors, for 
+   cool transparency =-) */
+/* #undef HAVE_USE_DEFAULT_COLORS */
+
+/* Define this if you have the linux consolechars program */
+#define HAVE_CONSOLECHARS /usr/bin/consolechars
+
+/* Define this if you have the linux setfont program */
+/* #undef HAVE_SETFONT */
+
+/* Define this if you have the wresize function in your ncurses-type library */
+/* #undef HAVE_WRESIZE */
+
+/* Define this if you have the resizeterm function in your ncurses-type library */
+/* #undef HAVE_RESIZETERM */
+
+/* Define if you have the putenv function.  */
+#define HAVE_PUTENV 1
+
+/* Define if you have the <curses.h> header file.  */
+/* #undef HAVE_CURSES_H */
+
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <ncurses.h> header file.  */
+/* #undef HAVE_NCURSES_H */
+
+/* Define if you have the <sys/ioctl.h> header file.  */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define if you have the <termio.h> header file.  */
+#define HAVE_TERMIO_H 1
+
+/* Define if you have the <termios.h> header file.  */
+#define HAVE_TERMIOS_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the ncurses library (-lncurses).  */
+/* #undef HAVE_LIBNCURSES */
+
+/* Name of package */
+#define PACKAGE "cmatrix"
+
+/* Version number of package */
+#define VERSION "1.2a"
+
