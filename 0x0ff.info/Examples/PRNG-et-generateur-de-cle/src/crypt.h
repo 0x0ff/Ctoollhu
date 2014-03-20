b45eb544e587db0ff3da52b41e4c91675d29a5f1
0x0ff <postmaster@0x0ff.info>
2014-03-20 11:29:07 +0100

    Create crypt.h

---
+++ 0x0ff.info/Examples/PRNG-et-generateur-de-cle/src/crypt.h
@@ -0,0 +1,13 @@
+
+#include <openssl/bio.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <openssl/md5.h>
+#include <math.h>
+#include "init.h"
+
+int HKDF(const char * Ks,const char *Ns, int p_i, char *Kfinal[HASH_SIZE]);
+int Base64Encode(const char *m, char **buffer);
+int Base64Decode(char *m, char **buffer);
+void sha256(const char *m, char *hash[65]);
+void hash_function(const char *m, char **hash);
